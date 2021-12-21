4c2f8e5f3626577a6fcd3d7cfcb5f36c4534facc
rvpatil1432 <rvpatil1432@gmail.com>
2021-12-21 08:23:04 +0530

    Added : welcome to Hotel reservation program

---
+++ src/main/java/com/bridgelabz/AdvJavaWorkshop/HotelReservation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.AdvJavaWorkshop;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+
+	}
+
+}
